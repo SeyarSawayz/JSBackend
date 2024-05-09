864b79feda2e25b690524ce8bf2dd40f51dff61d
seyar <seyarsawayz16.ss@gmail.com>
2024-05-09 18:03:12 +0530

    All setups configured

--- src/app.js
+++ src/app.js
@@ -9,4 +9,8 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+//routes
+import { router } from "./routes/user.routes.js";
+
+app.use("/api/v1/user", router);
 export { app };
