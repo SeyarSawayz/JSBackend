d2775a3047d7d99744bf707a5d6c67eeaaeac5b9
Seyar Sawayz <seyarsawayz16.ss@gmail.com>
2024-05-09 23:03:53 +0530

    data validation checked

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,14 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiErrors } from "../utils/ApiErrors.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "kayhanTech",
-  });
+  const { email, fullName, username, password } = req.body;
+  console.log("email", email);
+  if (
+    [fullName, username, email, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiErrors(400, "All Fields are required");
+  }
 });
 
 export { registerUser };
